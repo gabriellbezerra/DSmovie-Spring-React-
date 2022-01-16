dc4eb8c717a4f20e0b3c1e5d36ef23ba85bc9f1b
Gabriel Bezerra <ackermanngabriell@gmail.com>
2022-01-16 17:14:52 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
+import { validateEmail } from 'utils/validate';
 import './styles.css'
 
 type Props = {
@@ -20,12 +21,39 @@ function FormCard( { movieId } : Props) {
             });
     }, [movieId]);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+
+        if (!validateEmail(email)){
+            return;
+        }
+
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            console.log(response.data)
+        });
+
+    }
+
   return (
     <div className="dsmovie-form-container">
       <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="dsmovie-form">
+        <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
@@ -33,11 +61,11 @@ function FormCard( { movieId } : Props) {
           <div className="form-group dsmovie-form-group">
             <label htmlFor="score">Informe sua avaliação</label>
             <select className="form-control" id="score">
-              <option>1 (∗)</option>
-              <option>2 (∗∗)</option>
-              <option>3 (∗∗∗)</option>
-              <option>4 (∗∗∗∗)</option>
-              <option>5 (∗∗∗∗∗)</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
             </select>
           </div>
           <div className="dsmovie-form-btn-container">
